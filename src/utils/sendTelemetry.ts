2b65129537ad53989ff04452c4be67fd90f003a8
Joao Pedro <devjpe@gmail.com>
2024-07-24 22:28:47 -0300

    Remove: request in sendTelemetry

--- src/utils/sendTelemetry.ts
+++ src/utils/sendTelemetry.ts
@@ -10,16 +10,5 @@ export interface TelemetryData {
 }
 
 export const sendTelemetry = async (route: string): Promise<void> => {
-  const telemetry: TelemetryData = {
-    route,
-    apiVersion: `${packageJson.version}`,
-    timestamp: new Date(),
-  };
-
-  axios
-    .post('https://log.evolution-api.com/telemetry', telemetry)
-    .then(() => {})
-    .catch((error) => {
-      console.error('Telemetry error', error);
-    });
+  return;
 };
